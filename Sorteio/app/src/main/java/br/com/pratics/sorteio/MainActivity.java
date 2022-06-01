52a4766258ebac78f11184235b41b89b35c23cc9
Samuel Lopes <samek.vitor1@gmail.com>
2022-05-31 22:57:43 -0300

    APRENDENDO CLIQUE EM BOTÃO

--- Sorteio/app/src/main/java/br/com/pratics/sorteio/MainActivity.java
+++ Sorteio/app/src/main/java/br/com/pratics/sorteio/MainActivity.java
@@ -3,6 +3,8 @@ package br.com.pratics.sorteio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void alterarTexto(View view){
+
+        TextView texto = findViewById(R.id.pergunta1);
+        texto.setText("pergaminho");
     }
 }
